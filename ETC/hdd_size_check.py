4c2607a27cd2529901f814d7f5d83090ce7ac381
DESKTOP-54T5RVR\Lee ChangHyung <lch1000m@gmail.com>
2017-11-27 20:08:34 +0900

    1.03

--- ETC/hdd_size_check.py
+++
@@ -1,6 +0,0 @@
-
-import shutil
-
-res = shutil.disk_usage(r'D:')
-
-print(res[0]/1024/1024/1024/1024)
--- ETC/start_file.py
+++
@@ -1,4 +0,0 @@
-
-from os import startfile
-
-startfile(r'D:\ChangHyung\영화\무라이\download\LOVE-356 1.83G.torrent')
